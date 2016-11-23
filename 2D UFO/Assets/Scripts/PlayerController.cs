6d6e0e77b82bc64143c088aae8aa9e0856cdc038
Pedro Brandao <pcrbrandao@gmail.com>
2016-11-23 11:33:47 -0200

    Concluido!

---
+++ 2D UFO/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody2D rb2d;
+	private int count;
+
+	void Start() {
+
+		rb2d = GetComponent<Rigidbody2D> ();
+		count = 0;
+		winText.text = "";
+		SetCountText ();
+	}
+
+	void FixedUpdate() {
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertial = Input.GetAxis ("Vertical");
+
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertial);
+		rb2d.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		
+		if (other.gameObject.CompareTag ("PickUp")) {
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText() {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You win!";
+		}
+	}
+}
