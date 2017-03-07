b84489ee7139d6c65567f5e3960615c9230c9797
Pedro Brandao <pcrbrandao@gmail.com>
2017-03-07 09:10:48 -0300

    em andamento

---
+++ MainMenu/Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // precisa incluir isso aqui
+
+
+// Apenas carrega a cena do index que está na build settings
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+	public void LoadByIndex(int sceneIndex) {
+		
+		SceneManager.LoadScene (sceneIndex);
+	}
+}
