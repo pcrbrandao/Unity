56c878e6d28b950be4203d34f88cbde5f995d4dd
Pedro Brandao <pcrbrandao@gmail.com>
2017-07-18 13:47:52 -0300

    terminado RaycastShoot-07-2017

--- RaycastShoot-07-2017/Assets/Let's Try Shooter/Scripts/RaycastShoot.cs
+++ RaycastShoot-07-2017/Assets/Let's Try Shooter/Scripts/RaycastShoot.cs
@@ -6,15 +6,56 @@ public class RaycastShoot : MonoBehaviour {
 
 	public int gunDamage = 1;
 	public float fireRate = .25f;
-	public float weaponRange
+	public float weaponRange = 50f;
+	public float hitForce = 100;
+	public Transform gunEnd;
+
+	private Camera fpsCam;
+	private WaitForSeconds shotDuration = new WaitForSeconds (.07f);
+	private AudioSource gunAudio;
+	private LineRenderer laserLine;
+	private float nextFire;
 
 	// Use this for initialization
 	void Start () {
-		
+		laserLine = GetComponent<LineRenderer> ();
+		gunAudio = GetComponent<AudioSource> ();
+		fpsCam = GetComponentInParent<Camera> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetButtonDown ("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			StartCoroutine (ShotEffect ());
+
+			Vector3 rayOrigin = fpsCam.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, 0.0f));
+			RaycastHit hit;
+			laserLine.SetPosition (0, gunEnd.position);
+
+			if (Physics.Raycast (rayOrigin, fpsCam.transform.forward, out hit, weaponRange)) {
+				laserLine.SetPosition (1, hit.point);
+
+				ShootableBox helth = hit.collider.GetComponent<ShootableBox> ();
+
+				if (helth != null) {
+					helth.Damage (gunDamage);
+				}
+
+				if (hit.rigidbody != null) {
+					hit.rigidbody.AddForce (-hit.normal * hitForce);
+				}
+
+			} else {
+				laserLine.SetPosition (1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+			}
+		}
+	}
+
+	private IEnumerator ShotEffect() {
+		gunAudio.Play ();
+		laserLine.enabled = true;
+		yield return shotDuration;
+		laserLine.enabled = false;
 	}
 }
